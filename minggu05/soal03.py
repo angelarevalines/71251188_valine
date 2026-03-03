be52b0430126f23b6b09f15468ad270a2228d2cf
71251188 - angelarevalines <angelarevaline@gmail.com>
2026-03-03 14:40:24 +0700

    Update soal03.py

--- minggu05/soal03.py
+++ minggu05/soal03.py
@@ -2,6 +2,6 @@
 celcius_to_fahrenheit = lambda C : (9/5) * C + 32
 celcius_to_reamur = lambda C : 0.8 * C
 
-print(celcius_to_fahrenheit(100))
-print(celcius_to_reamur(80))
-print(celcius_to_fahrenheit(0))
+print("Fahrenheit: ", celcius_to_fahrenheit(100))
+print("Reamur: ", celcius_to_reamur(80))
+print("Fahrenheit: ", celcius_to_fahrenheit(0))
