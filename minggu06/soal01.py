d8fb856923bc26b4a6ad84eb596749d19029c312
71251188 - angelarevalines <angelarevaline@gmail.com>
2026-03-13 10:21:04 +0700

    Update soal01.py

--- minggu06/soal01.py
+++ minggu06/soal01.py
@@ -1,6 +1,10 @@
-def perkalian(a, b):
+def perkalian(a,b):
     hasil = (a * b)
-    proses = " + ".join([str(b)]* a)
+    proses = ""
+    for i in range(1, a + 1):
+        proses += str(b)
+        if i < a:
+            proses += " + "
     print(f"{a} x {b} = {proses} = {hasil}")
     
 perkalian(6, 5)
